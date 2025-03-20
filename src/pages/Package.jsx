7952c7620d4767963bbde0ec895bcecd0ced44bf
Micaela <micaela.zampone@jedify.io>
2025-03-20 16:01:10 -0300

    se agregan clips en carrusel , imagenes y se acomoda img zeus apuntando

--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,16 +1,16 @@
 import { ColumnBg } from "../components/sectionThree/ColumnBg";
+import  Steps  from "../components/sectionThree/Steps";
 import Guarantee from "../components/sectionTwo/Guarantee";
-import UnblockNow from "../components/UnblockNow";
-
 
 export function Package(){
 
-    return(<>
-    <UnblockNow/>
-    <UnblockNow/>
+    return(
+    <>
+    <Steps />
     <ColumnBg/>
     <div className="bg-olimpusBlue w-full p-1 ">
             <Guarantee />
     </div>
-    </>)
+    </>
+    )
 }
